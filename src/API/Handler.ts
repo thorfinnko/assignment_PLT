64d42e1bf0cf5f7a8a0d2dc26ab89bf26dcb05e6
vikram <vikramxagarwal1124@gmail.com>
2023-09-28 18:52:46 +0530

    minor fix

--- src/API/Handler.ts
+++ src/API/Handler.ts
@@ -1,13 +1,4 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-// import normalizeHeaderName from 'axios/lib/helpers/normalizeHeaderName';
-import utils from 'axios/lib/utils';
-
-function setContentTypeIfUnset(headers: any, value: string) {
-  const apiHeaders = headers;
-  if (!utils.isUndefined(apiHeaders) && utils.isUndefined(apiHeaders['Content-Type'])) {
-    apiHeaders['Content-Type'] = value;
-  }
-}
 
 const axiosInstance = axios.create({
   baseURL: 'https://my-json-server.typicode.com/benirvingplt/products',
@@ -33,36 +24,10 @@ axiosInstance.interceptors.request.use(
     apiConfig.transformRequest = [
       (data, headers) => {
         const apiData = data;
-
-        // apiData = sanitizeMaster(data);
-        // normalizeHeaderName(headers, 'Content-Type');
-        if (
-          utils.isFormData(apiData) ||
-          utils.isArrayBuffer(apiData) ||
-          utils.isStream(apiData) ||
-          utils.isFile(apiData) ||
-          utils.isBlob(apiData)
-        ) {
-          return apiData;
-        }
-        if (utils.isArrayBufferView(apiData)) {
-          return apiData.buffer;
-        }
-        if (utils.isURLSearchParams(apiData)) {
-          setContentTypeIfUnset(headers, 'application/x-www-form-urlencoded;charset=utf-8');
-          return apiData.toString();
-        }
-        if (utils.isObject(apiData)) {
-          setContentTypeIfUnset(headers, 'application/json;charset=utf-8');
-          return JSON.stringify(apiData);
-        }
-
         return apiData;
       },
     ];
     return apiConfig;
-    // this condition will never hit
-    // return true;
   },
   (error) => Promise.reject(error)
 );
